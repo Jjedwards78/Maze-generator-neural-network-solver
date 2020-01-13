d6d0e62c3c11c94fdcfc0dc02720442ab78c1ae7
Jakob Edwards <jjedwards78@gmail.com>
2020-01-13 04:45:15 -0600

    got window working

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,13 +1,22 @@
 package game;
 
+import javax.swing.JFrame;
+
 public class Game implements Runnable {
 
-    public void run() {
+    private JFrame frame;
 
+
+    public void run() {
+        
     }
 
     public synchronized void start() {
-
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
     }
 
     public synchronized void stop() {
